dd48dc34dc9d665c915bfeb9ab2df37a055827a7
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2024-01-23 08:34:50 -1000

    Add files via upload

---
+++ VanBatenburg_Bobbie_week3_ICS110P.py
@@ -0,0 +1,31 @@
+#VanBatenburg, Bobbie
+#ICS 110P
+#Conditionals : if, if-else, if-elif-else
+'''
+my_name = 'Bobbie'
+user_name = input('Enter your name: ')
+
+if user_name == my_name :
+    print("Cool! That's my name too!")
+
+print('Goodbye!')
+'''
+'''
+username = input('Choose a username: ')
+password = input('Choose a password: ')
+
+if username != password:
+    print('Looks good!')
+'''
+'''
+my_age = 100
+user_age = int(input('Enter your age: '))
+
+if my_age > user_age :
+    print('hehe! You are a baby!')
+'''
+price_of_pizza = 12.99
+money_in_wallet = float(input('How much money do you have? $'))
+
+if money_in_wallet >= price_of_pizza:
+    print('You can buy a pizza!')
