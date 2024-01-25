1636805a2c6ed7804baabe87353076446fc821ec
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2024-01-25 08:21:42 -1000

    Add files via upload

--- ICS110P_Combining_Conditions.py
+++ ICS110P_Combining_Conditions.py
@@ -2,6 +2,26 @@
 # ICS 110P
 # Combining Conditions
 
+user_age = int(input('Enter your age: '))
+if 18 < user_age < 22:
+    print('You are a young adult.')
+elif 22 < user_age < 35:
+    print('You are STILL a young adult.')
+elif 35 < user_age < 50:
+    print('Dang, you are a grown-ass adult.')
+elif 50 < user_age < 65:
+    print('Are you getting the AARP mailers?')
+elif 65 < user_age < 80:
+    retired = input('Are you retired yet? (y/n)')
+    if retired == 'y':
+        print('Congratulations!')
+    else:
+        print('I hope you are retiring soon!')
+elif user_age >80:
+    print('The best years are yet to come!')
+else:
+    print('You are a baby!')
+'''
 # Declare variables with Nikki's information
 my_name = 'Nikki'
 my_age = 100
@@ -18,3 +38,4 @@ if my_name == user_name or my_age == user_age or user_fave_food == my_fave_food:
 else:
                print('We have nothing in common! :p')
 print('goodbye')
+'''
